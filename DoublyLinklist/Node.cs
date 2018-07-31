ffc30632645011c2955f31ef3112bee58c366a53
Mudkondwar, Sumit (Extranet) <Sumit.Mudkondwar@thomsonreuters.com>
2018-07-31 18:39:05 +0530

    Double link list

---
+++ DoublyLinklist/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinklist
+{
+    public class Node
+    {
+        public Node Previous;
+        public object Value;
+        public Node Next;
+    }
+}
