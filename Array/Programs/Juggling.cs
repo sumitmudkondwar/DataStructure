7a768c3bfe5a8044f117c437ac8d46cdde5ea3cf
Sumit Prakash Mudkondwar <SM00544033@techmahindra.com>
2018-07-31 17:52:42 +0530

    Add project files.

---
+++ Array/Programs/Juggling.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array.Programs
+{
+    public class Juggling
+    {
+        /* Function to left rotate arr[] of size n by d*/
+        public void leftRotate(int[] arr, int d, int n)
+        {
+            int i, j, k, temp;
+            for (i = 0; i < gcd(d, n); i++)
+            {
+                /* move i-th values of blocks */
+                temp = arr[i];
+                j = i;
+                while (true)
+                {
+                    k = j + d;
+                    if (k >= n)
+                        k = k - n;
+                    if (k == i)
+                        break;
+                    arr[j] = arr[k];
+                    j = k;
+                }
+                arr[j] = temp;
+            }
+        }
+
+        /*UTILITY FUNCTIONS*/
+        /* Function to print an array */
+        public void printArray(int[] arr, int size)
+        {
+            for (int i = 0; i < size; i++)
+                Console.Write(arr[i] + " ");
+        }
+
+        /* Fuction to get gcd of a and b*/
+        public int gcd(int a, int b)
+        {
+            if (b == 0)
+                return a;
+            else
+                return gcd(b, a % b);
+        }
+    }
+}
---
+++ OOPS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassA obj = new ClassB();
+            Console.WriteLine(obj.Method1());
+        }
+    }
+
+    public class ClassA
+    {
+        public virtual int Method1()
+        {
+            return 1;
+        }
+    }
+
+    public class ClassB : ClassA
+    {
+        public override int Method1()
+        {
+            return 2;
+        }
+    }
+}
